382fe12d139d22c31efb54bedf2ba73aa316972f
TalesSilva <tales.silva@gafit.com.br>
2022-07-23 01:52:30 -0300

    testes

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 const Forca = require("./forca");
 
-const jogo = new Forca("abacaxi");
+const jogo = new Forca("Espacos-em-branco-nao-sao-validos");
 
 while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) {
   const chute = readline.question("Aguardando chute: \n");
@@ -9,5 +9,5 @@ while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) {
 
   console.log(jogo.buscarDadosDoJogo());
 }
-
+jogo.desvendarPalavra();
 console.log("você " + jogo.buscarEstado());
--- desafio/forca.js
+++ desafio/forca.js
@@ -1,7 +1,7 @@
 class Forca {
   constructor(palavra) {
     this._palavra = palavra.toUpperCase().trim().split("");
-    this._palavraSecreta = "".padStart(palavra.length, "-").split("");
+    this._palavraSecreta = "".padStart(palavra.length, "_").split("");
     this._vidas = 6;
     this._letrasChutadas = [];
   }
@@ -32,42 +32,43 @@ class Forca {
   }
   chutar(letra) {
     //Pensei em por uma verificação de tipo também, mas achei que poderia limitar a brincadeira
+    console.log("________NOTIFICACAO________");
     let chute = letra.toUpperCase();
     if (chute.length != 1) {
-      return console.log("Você deve inserir uma letra por vez!");
+      return console.log("- Você deve inserir uma letra por vez!");
     } else if (this.getLetrasChutadas.includes(chute)) {
-      return console.log("Letra repetida. Por favor, tente outra!");
+      return console.log("- Letra repetida. Por favor, tente outra!");
     } else if (!this._palavra.includes(chute)) {
       this._vidas--;
       this._letrasChutadas.push(chute);
-      return console.log("PEM!!! Letra errada!");
+      return console.log("- PEM!!! Letra errada!");
     } else {
       //inserir troca de '-' por letras
       this._letrasChutadas.push(chute);
       for (let i = 0; i < this._palavra.length; i++) {
-        // console.log(this._palavraSecreta);
         if (chute == this._palavra[i]) {
           this._palavraSecreta.splice(i, 1, chute);
         }
       }
-      return console.log("Você acertou a letra!");
+      return console.log("- Você acertou a letra!");
     }
-    // console.log(this.palavra);
   }
 
   buscarEstado() {
-    console.log("ESTADO * * *");
+    console.log("\n__________________");
     // colocar lógica de alterar estado
-    if (!this._palavraSecreta.includes("-")) {
+    if (!this._palavraSecreta.includes("_")) {
       return "ganhou";
-    }
-    if (this._vidas > 0) {
+    } else if (this._vidas > 0) {
       return "aguardando chute";
     } else if (this._vidas == 0) {
       return "perdeu";
     }
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+
+  desvendarPalavra() {
+    return console.log(`PALAVRA MISTERIOSA: "${this._palavra.join("")}"`);
   }
-  // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
     const status = {
@@ -75,88 +76,36 @@ class Forca {
       vida: this._vidas,
       palavra: this._palavraSecreta.join(""),
     };
-    console.log("* * * STATUS JOGADOR * * *");
-    return console.log(
-      `Letra(s) chutadas: ${status.letraChutada}\nVida: ${status.vida}\nPalavra: ${status.palavra}`
+    console.log("\n______STATUS JOGADOR______");
+    console.log(
+      `| Letra(s) chutadas: ${status.letraChutada}\n| Vida: ${status.vida}\n| Palavra: ${status.palavra}`
     );
+    return "";
   }
 }
 
 module.exports = Forca;
 
 /*
-1. O jogo deve iniciar com 6 vidas
+----------REGRAS---------- 
+1. O jogo deve iniciar com 6 vidas - V
 
-2. O jogo deve iniciar com o estado `aguardando chute`.
+2. O jogo deve iniciar com o estado `aguardando chute`. - V
 
-3. Todo chute deve conter apenas uma letra, caso tenha mais de uma a jogada deve ser ignorada, ou seja, não deve alterar nenhum estado.
+3. Todo chute deve conter apenas uma letra, caso tenha mais de uma a jogada deve ser ignorada, ou seja, não deve alterar nenhum estado. - V
 
-4. Caso a letra chutada esteja errada mas já foi chutada anteriormente a jogada deve ser ignorada, ou seja, não deve alterar nenhum estado.
+4. Caso a letra chutada esteja errada mas já foi chutada anteriormente a jogada deve ser ignorada, ou seja, não deve alterar nenhum estado.  - V (Letras iguais certas seguirão a mesma lógica)
 
-5. Toda chamada ao método chutar deve registrar a letra em letrasChutadas
+5. Toda chamada ao método chutar deve registrar a letra em letrasChutadas - V
 
-6. Se a letra chutada não estiver contida na palavra, deve subtrair uma vida
+6. Se a letra chutada não estiver contida na palavra, deve subtrair uma vida  - V
 
-7. Se a letra chutada estiver contida na palavra, deve ser substituida na "palavra" em sua respectiva posição.
+7. Se a letra chutada estiver contida na palavra, deve ser substituida na "palavra" em sua respectiva posição. - V
 
-Ex.: A palavra secreta é "bala" e o jogador chutou a letra "b", então a palavra que é retornada no método buscarDadosDoJogo, deve ser ["b", "_", "_", "_" ].
+Ex.: A palavra secreta é "bala" e o jogador chutou a letra "b", então a palavra que é retornada no método buscarDadosDoJogo, deve ser ["b", "_", "_", "_" ]. - V (Achei que unindo ficaria mais bonito, visualmente falando)
 
-8. Caso a quantidade de vidas chegue a 0 (zero), o estado do jogo deve mudar para `perdeu`.
+8. Caso a quantidade de vidas chegue a 0 (zero), o estado do jogo deve mudar para `perdeu`. - V
 
-9. Caso a quantidade de vidas seja maior que zero e o jogador acerte a última letra, o estado do jogo deve mudar para `ganhou`.
-
-// let letrasChutadas = ["a", "b", "c"]; // Deve conter todas as letras chutadas
-    // let vidas = 6; // Quantidade de vidas restantes
-    // let palavra = ""; // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-    // let getLetras = function () {
-    //   return letrasChutadas;
-    // };
-    // let getVida = function () {
-    //   return vidas;
-    // };
-    // let getPalavra = function () {
-    //   return palavra;
-    // };
-    // let setLetras = function (value) {
-    //   return (letrasChutadas = value);
-    // };
-    // let setVida = function (value) {
-    //   return (vidas = value);
-    // };
-    // let setPalavra = function (value) {
-    //   return (palavra = value);
-    // };
-
-    // let metodos = {
-    //   getPalavra: getPalavra,
-    //   getVida: getVida,
-    //   getLetras: getLetras,
-    //   setPalavra: setPalavra,
-    //   setVida: setVida,
-    //   setLetras: setLetras,
-    // };
-
-    // Object.defineProperties(metodos, {
-    //   getPalavra: {
-    //     enumerable: false,
-    //   },
-    //   getVida: {
-    //     enumerable: false,
-    //   },
-    //   getLetras: {
-    //     enumerable: false,
-    //   },
-    //   setPalavra: {
-    //     enumerable: false,
-    //   },
-    //   setVida: {
-    //     enumerable: false,
-    //   },
-    //   setLetras: {
-    //     enumerable: false,
-    //   },
-    // });
-
-    // return metodos;
+9. Caso a quantidade de vidas seja maior que zero e o jogador acerte a última letra, o estado do jogo deve mudar para `ganhou`. - V
 
 */
